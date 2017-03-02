29342bd25cd152492dfd78e027ca6f7618b2d09a
Ivan Prokudin <iprok@kinetiksoft.com>
2017-03-02 12:59:07 +0300

    updated select2 to 4.0.3

--- lib/hotkeys_plugin/hotkeys_hooks.rb
+++ lib/hotkeys_plugin/hotkeys_hooks.rb
@@ -6,8 +6,8 @@ module HotkeysPlugin
         <<-TAGS
           #{stylesheet_link_tag 'hotkeys', :plugin => 'redmine_hotkeys_js'}
           #{javascript_include_tag 'hotkeys', :plugin => 'redmine_hotkeys_js'}
-          <link href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.2/css/select2.min.css" rel="stylesheet" />
-			<script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.2/js/select2.min.js"></script>
+          <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
         TAGS
       end
 
